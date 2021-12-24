4f1b975546c7291638f49a7ce0e78980d4a22951
J. Rockford <jrf@mbn.nifty.com>
2021-12-24 17:25:09 +0900

    update: v0.18

--- jrf_semaphore.pl
+++ jrf_semaphore.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/perl
 #require 5.008_003;
 { package main;
-  my $TS = 'Time-stamp: <2020-11-30T18:34:08Z>';
+  my $TS = 'Time-stamp: <2021-12-24T08:06:16Z>';
   $TS =~ s/Time-stamp\:\s+<(.*)>/$1/;
   my $AUTHOR = "JRF (http://jrf.cocolog-nifty.com/)";
-  our $VERSION = "0.17; jrf_semaphore.pl; last modified at $TS; by $AUTHOR";
+  our $VERSION = "0.18; jrf_semaphore.pl; last modified at $TS; by $AUTHOR";
   our $DEBUG = 1;
 }
 
